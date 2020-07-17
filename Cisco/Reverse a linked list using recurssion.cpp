9cdcc21733833b132f9a2ef45507947584ff9e86
AdityaGattu <52188412+AdityaGattu@users.noreply.github.com>
2020-07-17 21:50:54 +0530

    Update Reverse a linked list using recurssion.cpp

--- Cisco/Reverse a linked list using recurssion.cpp
+++ Cisco/Reverse a linked list using recurssion.cpp
@@ -14,10 +14,10 @@ public:
         
         if(head==NULL || head->next==NULL){return head;}
         
-        ListNode* newnode=reverseList(head->next);
+        ListNode* newnode=reverseList(head->next); // traverse till last node and store pointer to new node(final head after reversing) ;
         
-        head->next->next=head;
-        head->next=NULL;
+        head->next->next=head;  //if(head->next !=NULL reversing the direction of list) 
+        head->next=NULL;        //for last node head->next must be null after reversing;
        
         return newnode;
     }
