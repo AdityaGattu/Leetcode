ad9f22473e9033048b095760a2feacf46565d7a0
AdityaGattu <52188412+AdityaGattu@users.noreply.github.com>
2020-07-25 21:48:15 +0530

    Leetcode - hard

---
+++ Amazon/41. First Missing Positive.cpp
@@ -0,0 +1,30 @@
+// every index should be matched with value ....here since started with 0 ... index should be matched with value index+1
+//negitive no no can be ignored
+//basically index 1shou;d have 1 ,2 should have to 2 ....so on....
+//in code since started from  index0 ->should have 1 ...and so on.. 
+          
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            int n=nums.size();
+            
+            while( nums[i]>=1 && nums[i]<=n && nums[i] != nums[ nums[i] -1 ] )  
+            {
+                swap(nums[i],nums[nums[i]-1]);
+            }
+            
+        }
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            if( nums[i]!= i+1 )
+            {return i+1;}
+        }
+        
+        return nums.size()+1;
+        
+    }
+};
