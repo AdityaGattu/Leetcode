225f5852266588f76c10295fcbe8e3596b232205
AdityaGattu <52188412+AdityaGattu@users.noreply.github.com>
2020-08-23 15:25:29 +0530

    Update Segregate 0s and 1s.cpp

--- Goldman Sachs/Segregate 0s and 1s.cpp
+++ Goldman Sachs/Segregate 0s and 1s.cpp
@@ -1,3 +1,56 @@
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	//code
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    vector<int>v;
+	    for(int i=0;i<n;i++)
+	    {
+	        int x;
+	        cin>>x;
+	        v.push_back(x);
+	    }
+	    int l=0;int r=n-1;
+	    while(l<r)
+	    {
+	        while(v[l]==0 && l<r)
+
+		{
+	            l++;
+	        }
+	        while(v[r]==1 && l<r)
+	        {
+	            r--;
+	        }
+	        if(l<r)
+	        {
+	            v[l]=0;
+	            v[r]=1;
+	            l++;
+	            r--;
+	        }
+	    }
+	     for(int i=0;i<n;i++)
+	    {
+	        cout<<v[i]<<" ";
+	    }
+	    
+	    cout<<"\n";
+	    
+	    
+	}
+	return 0;
+}
+************************************************
 #include <iostream>
 using namespace std;
 
