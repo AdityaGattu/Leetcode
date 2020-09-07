24bffedecdb47cb674d8ab99801d34b633096740
AdityaGattu <52188412+AdityaGattu@users.noreply.github.com>
2020-09-07 21:33:30 +0530

    innovative question

---
+++ Microsoft/Longest Substring Without 3 Contiguous Occurrences of Letter.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string longest_substring(string s){
+    
+    int cnt=1;
+    int start=0;
+    int curr_start;
+    int max_length=-99999;
+    for(int i=1;i<s.length();i++)
+    {
+        if(s[i]==s[i-1])
+        {
+            cnt++;
+        }
+        
+        else
+        {
+            cnt=1;
+        }
+        
+        if(cnt<=2)
+        {
+            if(i-start+1 > max_length)
+            {
+                max_length=i-start+1;
+                curr_start=start;
+            }
+        }
+        else
+        {
+            start=i-1;
+            cnt=2;
+        }
+        
+    }
+   return s.substr(curr_start,max_length);
+}
+int main() {
+	string result = longest_substring("aabbaaaaabb");
+	cout<<result<<endl;
+	return 0;
+}
